269ad12e4514e322e19c974864b40d0a24a1aa12
S.J MANDAL <142606709+sofiurjamanmandal@users.noreply.github.com>
2024-03-22 13:03:06 +0000

    dataType

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//  alert(3+3) // we are using nodejs not browser
+
+console.log("Rony");
+
+let name = "Rony"
+let age = 24
+let isLoggedIn = false
+
+
+// number=> 2 power 53
+// Bigin 
+// string => ""
+// boolean true/ false
+// null=> standalone value
+// undefined =>
+// symbol => unique
+
+
+// Object 
+
+console.log(typeof null); //Object
+console.log(typeof undefined);// undefined
