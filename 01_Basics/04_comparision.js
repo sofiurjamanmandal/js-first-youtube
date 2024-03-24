b9d9b5bd4b02bcee950952eef9d2d550aa30f02a
S.J MANDAL <142606709+sofiurjamanmandal@users.noreply.github.com>
2024-03-24 02:05:26 +0000

    compare

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0)
+                                 // >>>>> it's confusing method don't try 
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === strict check     it's right way to check original value 
+
+console.log("2" === 2);
