7ba1d9e90b3f9f73f9a0ac1a74c7086a3d40187e
S.J MANDAL <142606709+sofiurjamanmandal@users.noreply.github.com>
2024-03-24 01:37:05 +0000

    prefix & postfix

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
- console.log(typeof valueInNumber);
- console.log(valueInNumber);
+// let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
 
 
  // "33" => 33
@@ -26,5 +26,26 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+
+// ****************** Operations*****************
+
+
+let value = 3
+
+let negvalue = -value
+// console.log(1 + 2)
+// console.log(1 + 3)
+// console.log("1" + 2 + 2) // first string assign to all read string
+// console.log(1 + 2 + "2")  // first addition and then string
+
+// console.log(negvalue);
+// console.log((3 + 4) * 5 % 3);
+
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter); // study link:- https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
