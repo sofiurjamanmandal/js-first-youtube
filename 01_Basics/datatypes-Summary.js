f8e5677932df4fdde8758a2ec48414afff3d3506
S.J MANDAL <142606709+sofiurjamanmandal@users.noreply.github.com>
2024-03-24 03:20:45 +0000

    datatype Summary

---
+++ 01_Basics/datatypes-Summary.js
@@ -0,0 +1,44 @@
+// javascript datatype
+
+// primitive
+// 7 type    :- String, Number, boolean, Null, Undefined, Symbol, BigInt.
+
+const score = 100   // Number
+const scoreValue = 100.3 // Number 
+
+const isLoggedIn = false // boolean
+
+const OutsideTemp = null // null
+
+let userEmail; // Undefined
+
+const id = Symbol('123')
+ const anotherId = Symbol('123') // it's total defrent two symbol
+
+//  console.log(id === anotherId)
+
+//  const bigNumber = 3456543576654356754n
+//  console.log(typeof bigNumber)
+
+
+
+// Reference ( non-primitive)
+//  3 type  :- Array, Object, Functions.
+
+const hero = ["ironman ",  "capamerica", "Thor" ]; // Array defination
+
+let Obj = {    // Object defination
+    name:"Rony",
+    age:22,
+
+
+}
+
+ const myFunctions = function () {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
